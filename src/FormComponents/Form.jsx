255819cb26fffb2746a02e7eb3c71e5b399c2028
LiZz <fitz-i@foxmail.com>
2019-10-12 18:16:29 +0800

    增加：表单校验功能
    TODO:  结构调整

--- src/FormComponents/Form.jsx
+++ src/FormComponents/Form.jsx
@@ -5,33 +5,39 @@ import usePubSub from './usePubSub';
 
 function Form(props) {
   const { children, onChange, values } = props;
+  // 用来记录当前发生改变的表单的值，这里不能使用异步的useState
   const update = useRef({});
   const [internalValues, setInternalValues] = useState({});
   const [publish, subscribe] = usePubSub();
-
+  // 判断form是否受控
   const isUnderControll = useMemo(
     () => Object.prototype.hasOwnProperty.call(props, 'values'),
     [props]
   );
 
+  // 添加一个表单域
   const addItem = useCallback(
     (name, item) => {
+      // 订阅依赖
       const unsubscribes = item.dependence.map(depName =>
         subscribe(depName, (...subValues) => {
+          // TODO: item.onDepChange 可能只是想监听变化做其他操作？
+          // 并不需要返回值。如何处理？
           update.current = {
             ...update.current,
             [name]: item.onDepChange(...subValues),
           };
         })
       );
-
       return () => {
+        // 取消订阅
         unsubscribes.forEach(unsubscribe => unsubscribe());
       };
     },
     [subscribe]
   );
 
+  // 表单域的值发生改变
   const onItemChange = useCallback(
     (name, itemValue) => {
       update.current = { [name]: itemValue };
@@ -39,7 +45,6 @@ function Form(props) {
       publish(name, itemValue);
 
       if (isUnderControll) {
-        console.log(values, update.current);
         onChange({ ...values, ...update.current });
       } else {
         setInternalValues(prev => {
@@ -55,6 +60,7 @@ function Form(props) {
     [isUnderControll, onChange, publish, values]
   );
 
+  // context 传值
   const contextValue = useMemo(
     () => ({
       addItem,
--- src/FormComponents/FormItem.jsx
+++ src/FormComponents/FormItem.jsx
@@ -1,18 +1,33 @@
-import React, { useContext, useCallback, useEffect } from 'react';
+import React, { useContext, useCallback, useEffect, useState } from 'react';
 import propTypes from 'prop-types';
 import Context from './context';
 
-function FormItem({ children, filter, name, dependence, onDepChange }) {
+function FormItem({
+  children,
+  filter,
+  name,
+  dependence,
+  onDepChange,
+  validator,
+  oneOf,
+}) {
   const { onItemChange, addItem, values: formValues } = useContext(Context);
+  // render children props
   const isFunctionChildren = typeof children === 'function';
+  // 当前表单域的value
+  const internalValue = formValues[name];
+  // 校验信息
+  const [validateResult, setValidateResult] = useState(null);
 
   useEffect(() => {
+    // 添加\注销
     return addItem(name, {
       dependence,
       onDepChange,
     });
   }, [addItem, dependence, name, onDepChange]);
 
+  // onChange
   const handleChange = useCallback(
     (...values) => {
       let subValue;
@@ -28,10 +43,61 @@ function FormItem({ children, filter, name, dependence, onDepChange }) {
     [filter, name, onItemChange]
   );
 
+  // 校验输入
+  const validate = useCallback(() => {
+    let result = validator(internalValue, {
+      ...formValues,
+    });
+    let boolResult = true;
+    // -1 如果未返回值， 则判定为 true
+    if (typeof result === 'undefined') {
+      boolResult = true;
+      result = true;
+    }
+    // -2 如果返回的是布尔值，直接返回
+    else if (typeof result === 'boolean') {
+      boolResult = result;
+    }
+    // -2 返回了除了以上的任何值，判定为false
+    else {
+      boolResult = false;
+    }
+    setValidateResult(result);
+    return boolResult;
+  }, [formValues, internalValue, validator]);
+
+  const hideTip = useCallback(() => {
+    if (validateResult === null) {
+      return;
+    }
+    setValidateResult(null);
+  }, [validateResult]);
+
+  const clearState = useCallback(() => {
+    if (!oneOf) {
+      hideTip();
+      return;
+    }
+    this.props.form.items.forEach(item => {
+      if (oneOf.indexOf(item.props.name) > -1) {
+        item.hideTip();
+      }
+    });
+    hideTip();
+  }, [hideTip, oneOf]);
+
   return isFunctionChildren
-    ? children(formValues[name], handleChange)
+    ? children(internalValue, handleChange, {
+        validateResult,
+        validate,
+        clearState,
+      })
     : React.cloneElement(children, {
         ...children.props,
+        value: internalValue,
+        onChange: handleChange,
+        onBlur: validate,
+        onFocus: clearState,
       });
 }
 
